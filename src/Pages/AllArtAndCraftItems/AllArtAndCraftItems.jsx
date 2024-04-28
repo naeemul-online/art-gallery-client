20f21d9cc0c32d248f31abe4108408de3dd3a274
naeemul-online <naeemul.online@gmail.com>
2024-04-28 18:31:36 +0300

    All art and craft items data loaded

--- src/Pages/AllArtAndCraftItems/AllArtAndCraftItems.jsx
+++ src/Pages/AllArtAndCraftItems/AllArtAndCraftItems.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
+
 
 
 const AllArtAndCraftItems = () => {
+
+    const [item, setItem] = useState([])
+
+
+
+    useEffect(()=> {
+        fetch("http://localhost:5000/allArtAndCraftItem/")
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            setItem(data)
+        })
+    }, [item])
+    // console.log(item)
     return (
         <div>
-            <h2 className="text-3xl">All art and craft items</h2>
+            <h2 className="text-3xl">All art and craft items length is: {item.length}</h2>
+
+            {
+                item.map(p => <div key={p._id}>
+
+                    <h2>Hello Crafts: {p.itemName}</h2>
+                </div>)
+            }
         </div>
     );
 };
